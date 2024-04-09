1bd3bc4dd0689fd920f54cb5ba1b6a5903b7d8ea
Михаил <mporsev@mail.ru>
2024-04-09 22:40:13 +0500

    добавил возможность комментирования

--- API/API/Controllers/Test2Controller.cs
+++ API/API/Controllers/Test2Controller.cs
@@ -1,3 +1,4 @@
+using Microsoft.AspNetCore.Http.HttpResults;
 using Microsoft.AspNetCore.Mvc;
 
 namespace API.Controllers
@@ -6,10 +7,34 @@ namespace API.Controllers
     [Route("[controller]")]
     public class Test2Controller : ControllerBase
     {
-        [HttpGet("")]
-        public ActionResult Get()
+        public static List<Comment> Comments = new();
+        
+        [HttpPost("")]
+        public ActionResult AddComm([FromBody] Comment comment)
         {
+            Comments.Add(comment);
             return Ok();
         }
+        
+        [HttpGet("")]
+        public ActionResult<List<Comment>> Get()
+        {
+            return Ok(Comments);
+        }
+        [HttpDelete("")]
+        public ActionResult DeleteComm([FromQuery] int Id)
+        {
+            var commToDelete = Comments.FirstOrDefault(s => s.Id == Id);
+            Comments.Remove(commToDelete);
+            return Ok();    
+        }
+        
+        public class Comment
+        {
+            public int Id { get; set; }
+            public string Text { get; set; }
+            public string Author { get; set; }
+        }
+        
     }
 }
